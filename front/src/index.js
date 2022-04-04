34894ce273218ad11a6588e0bf4bc8963d48c8f1
Adler Coelho <adler.coelho.santos@icloud.com>
2022-04-04 07:01:08 -0300

    feat: (wip) create project structure

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { GlobalStyle } from './assets/styles/global';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <GlobalStyle />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
