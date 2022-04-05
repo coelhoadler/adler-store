216c6df45ad1e6668bb9a37877d9b2623d64360f
Adler Coelho <adler.coelho.santos@icloud.com>
2022-04-05 07:18:33 -0300

    feat: (wip) using axios lib on express

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const axios = require("axios").default;
+
 router.get("/", function (req, res, next) {
   res.status(200).json({ error: false });
 });
@@ -9,6 +11,10 @@ router.get("/api/items", function (req, res, next) {
   const search = req.query.search.trim() || '';
 
   if (search) {
+    axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${search}`).then(results => {
+      console.log('produtos', results.toString());
+    })
+
     res.status(200).json({ error: false });
   } else {
     res.status(502).json({ error: true });
